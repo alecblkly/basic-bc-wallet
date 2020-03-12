8122fd2835f41b125871724a43d4cb14b5832fe2
Alec Blakeley <alecblkly@gmail.com>
2020-03-12 19:09:26 -0400

    Working through to properly display transaction information.

--- basic-bc-wallet/src/App.js
+++ basic-bc-wallet/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
+import Transactions from './Components.js/Transactions';
 
 function App() {
-   return <div className="App">Basic BC Wallet</div>;
+   return (
+      <div className="App">
+         Basic BC Wallet
+         <Transactions />
+      </div>
+   );
 }
 
 export default App;
